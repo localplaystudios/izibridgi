219de95475d1e5371e465fa4ce090baca87e6e07
weenkom4ik3 <localplaystudiosconnect@gmail.com>
2026-09-12 19:03:57 +0300

    Update and rename EppaniyFly.lua to EbanniyFly.lua

--- EbanniyFly.lua
+++ EbanniyFly.lua
@@ -53,7 +53,7 @@ title.Size = UDim2.new(1, 0, 1, 0)
 title.BackgroundTransparency = 1
 title.Font = Enum.Font.GothamBold
 title.TextSize = 18
-title.Text = "✈ EPPANIY FLY"
+title.Text = "✈ EBANNIY FLY"
 title.TextColor3 = Color3.fromRGB(255, 255, 255)
 title.ZIndex = 11
 
