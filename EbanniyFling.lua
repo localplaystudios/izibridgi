4e5a5f58bcb262f92de6b08786fc61c5d5c270d8
weenkom4ik3 <localplaystudiosconnect@gmail.com>
2026-09-12 19:03:26 +0300

    Update and rename EpanniyFling.lua to EbanniyFling.lua

--- EbanniyFling.lua
+++ EbanniyFling.lua
@@ -52,7 +52,7 @@ title.Size = UDim2.new(1, 0, 1, 0)
 title.BackgroundTransparency = 1
 title.Font = Enum.Font.GothamBold
 title.TextSize = 18
-title.Text = "🤙 EPANNIY FLING"
+title.Text = "🤙 EBANNIY FLING"
 title.TextColor3 = Color3.fromRGB(255, 255, 255)
 title.ZIndex = 11
 
